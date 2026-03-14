ba4cd241e027f7cbfc6819f574f746d4ffde3ff2
Sudipto Roy <roy.sudipto15@gmail.com>
2026-03-14 08:54:01 +0530

    Refactor print statement to use f-string formatting

--- trailer_validation.py
+++ trailer_validation.py
@@ -2,4 +2,4 @@ movie_name = "Hereditary"
 channel_name = "A24"
 verified = True
 
-print(movie_name, "trailer from", channel_name, "verified:", verified)
+print(f"{movie_name} trailer from {channel_name} verified: {verified}")
